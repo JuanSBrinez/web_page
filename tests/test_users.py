e75b12358cb20ecd7ced100e10a0db756d4d6f3b
juan brinez <juan.brinez@loras.edu>
2021-07-13 03:26:47 +0000

    fifth commit

--- tests/test_users.py
+++ tests/test_users.py
@@ -27,6 +27,14 @@ class UsersTests(unittest.TestCase):
     def test_valid_user_registration(self):
         response = self.register('bob', 'bob@example.com', '12345')
         self.assertEqual(response.status_code, 200)
+        
+    def test_invalid_username_registration(self):
+        response = self.register('t', 'test@example.com', 'FlaskIsAwesome')
+        self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
+
+    def test_invalid_email_registration(self):
+        response = self.register('test2', 'test@example', 'FlaskIsAwesome')
+        self.assertIn(b'Invalid email address.', response.data)
 
 if __name__ == "__main__":
     unittest.main()
